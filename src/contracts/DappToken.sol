4bf8039c07507dd17d185cbd61f8ca84cb03f40b
Henelllis <Hellis1001@gmail.com>
2021-10-31 12:18:13 -0400

    updated pragma interfaces for truffle compile to work

--- src/contracts/DappToken.sol
+++ src/contracts/DappToken.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0;
 
 contract DappToken {
     string  public name = "DApp Token";
---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,11 @@
+pragma solidity >=0.5.0;
+
+contract TokenFarm {
+
+    string public name="Dapp Token Farm";
+
+    
+
+
+
+}
