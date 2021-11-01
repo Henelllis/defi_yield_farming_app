9b8f536c54996067d54ea79a0205b98d6bb6d6aa
Henelllis <Hellis1001@gmail.com>
2021-11-01 17:06:57 -0400

    constructor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,10 +1,18 @@
 pragma solidity >=0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+
 contract TokenFarm {
 
     string public name="Dapp Token Farm";
-
-    
+    DappToken public dappToken;
+    DaiToken public daiToken;
+    constructor(DappToken _dappToken, DaiToken _daiToken)  {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 
 
 
