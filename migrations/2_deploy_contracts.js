f2a2f6f46db78c32f05046ac72a46ad4151fb699
Henelllis <Hellis1001@gmail.com>
2021-11-01 09:01:29 -0400

    start fleshing out migration script and include constructor to TokenFarm

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,20 @@
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 const TokenFarm = artifacts.require("TokenFarm");
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+  //Deploy Mock Dai TOkens
+  await deployer.deploy(DaiToken);
+  const daiToken = await DaiToken.deployed();
+
+  //Deploy Dapp Token
+  await deployer.deploy(DappToken);
+  const dappToken = await DappToken.deployed();
+
+  //Deploy Token Farm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address);
+  const tokenFarm = await TokenFarm.deployed();
+
+  await dappToken.transfer(tokenFarm.address, 1000000000000000000000000);
+  // deployer.deploy(TokenFarm);
 };
