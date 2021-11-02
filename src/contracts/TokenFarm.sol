d8eaf065e0043b0bc9cd675270e130dace9c6f2b
Henelllis <Hellis1001@gmail.com>
2021-11-02 15:14:40 -0400

    staking tokens and unit tests for them

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -9,11 +9,45 @@ contract TokenFarm {
     string public name="Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+
+
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
     }
 
+    // 1. Stakes Tokens ( Deposit )
+    function stakeTokens(uint _amount) public {
+        //code goes in here
+
+        //transfer mock dai tokens to this contract for staking
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        //Update staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        //Add user to Stakers arr if and only if they haven't staked already
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+
+        //update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+
+
+
+    }
+
+    // 2. Unstaking Tokens ( Withdraw )
+
+    // 3. Issuing Tokens ()
 
 
 }
--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -8,17 +8,92 @@ require("chai")
   .use(require("chai-as-promised"))
   .should();
 
-contract("TokenFarm", (accounts) => {
-  let daiToken;
+function tokens(n) {
+  return web3.utils.toWei(n, "ether");
+}
+// contract("TokenFarm", (accounts) => {
+contract("TokenFarm", ([owner, investor]) => {
+  let daiToken, dappToken, tokenFarm;
+
   before(async () => {
+    //Load Contracts
     daiToken = await DaiToken.new();
+    dappToken = await DappToken.new();
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address);
+    //Transfer all Dapp Tokens to farm ( 1 million )
+    await dappToken.transfer(tokenFarm.address, tokens("1000000"));
+    //send tokens to investor
+    await daiToken.transfer(investor, tokens("100"), { from: owner });
+    // await daiToken.transfer(accounts[1], tokens("100"), { from: accounts[0] });
   });
 
   describe("mock DAI Deployment", async () => {
     it("has a name", async () => {
-      let daiToken = await DaiToken.new();
       const name = await daiToken.name();
       assert.equal(name, "Mock DAI Token");
+    });
+  });
+
+  describe("Dapp Token Deployment", async () => {
+    it("has a name", async () => {
+      const name = await dappToken.name();
+      assert.equal(name, "DApp Token");
+    });
+  });
+
+  describe("TokenFarm Deployment", async () => {
+    it("has a name", async () => {
+      const name = await tokenFarm.name();
+      assert.equal(name, "Dapp Token Farm");
+    });
+
+    it("contracts has tokens", async () => {
+      let balance = await dappToken.balanceOf(tokenFarm.address);
+
+      assert.equal(balance.toString(), tokens("1000000"));
+    });
+  });
+
+  describe.only("Farming Tokens", async () => {
+    it("rewards investors for staking mDai tokens", async () => {
+      let result;
+
+      //check investor balance before staking
+      result = await daiToken.balanceOf(investor);
+
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "Investor mock dai wallet balance correct before staking"
+      );
+
+      //Stake mock dai tokens
+      await daiToken.approve(tokenFarm.address, tokens("100"), {
+        from: investor,
+      });
+      await tokenFarm.stakeTokens(tokens("100"), { from: investor });
+
+      result = await daiToken.balanceOf(investor);
+      assert.equal(
+        result.toString(),
+        tokens("0"),
+        "Investor mock dai wallet balance correct after staking"
+      );
+
+      result = await tokenFarm.stakingBalance(investor);
+      assert.equal(
+        result.toString(),
+        tokens("100"),
+        "investor staking balance correct after staking"
+      );
+
+      result = await tokenFarm.isStaking(investor);
+
+      assert.equal(
+        result.toString(),
+        "true",
+        "investor staking status correct after staking"
+      );
     });
   });
 });
